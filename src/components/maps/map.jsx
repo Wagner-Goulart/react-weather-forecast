018e34ae5ebe28186de63e068cb88c8856d99414
Wagner <wagnergoulart0@gmail.com>
2023-10-28 19:13:23 -0300

    ADD: Componente de Map
    Signed-off-by: Wagner <wagnergoulart0@gmail.com>

---
+++ src/components/maps/map.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { useContext, useState } from "react";
+import { CityContext } from "../../contexts/cityContext";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
+
+function FlyToNewMapLocation({ latidude, longitude}) {
+    const map = useMap()
+
+    map.flyTo([latidude, longitude])
+    return null
+}
+
+function Map() {
+
+  const { city } = useContext(CityContext);
+  const [latitude, setLatitude] = useState();
+  const [longitude, setlongitude] = useState();
+
+  useEffect(() => {
+    if (city === undefined) {
+      return;
+    } else {
+      setLatitude(city[0].lat);
+      setlongitude(city[0].lon);
+    }
+  }, [city]);
+   
+    return (
+      <>
+        {latitude && longitude ? (
+          <MapContainer center={[latitude, longitude]} zoom={10}>
+            <FlyToNewMapLocation latidude={latitude} longitude={longitude}/>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[latitude, longitude]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        ) : (
+          "Digite uma cidade para buscar as informações"
+        )}
+      </>
+    );
+
+}
+
+export { Map }
--- src/pages/hero/hero.jsx
+++ src/pages/hero/hero.jsx
@@ -3,50 +3,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import "./hero.style.css";
-import { useContext, useState } from "react";
-import { CityContext } from "../../contexts/cityContext";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import { useEffect } from "react";
-import { latLng } from "leaflet";
-
+import { Map } from "../../components/maps/map";
 
 function Hero() {
-  const { city } = useContext(CityContext);
-  const [latitude, setLatitude] = useState();
-  const [longitude, setlongitude] = useState();
-
-  const position = [ latitude, longitude]
-
-  console.log(latitude);
-  console.log(longitude);
-
-  useEffect(() => {
-    if (city === undefined) {
-      return;
-    } else {
-      setLatitude(city[0].lat);
-      setlongitude(city[0].lon);
-    }
-  }, [city]);
-
+ 
   return (
     <section className="hero-section">
-      {city && city[0] && latitude && longitude ? (
-        <MapContainer center={position} zoom={10}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      ) : (
-        "Digite uma cidade para buscar as informações"
-      )}
+      < Map />
     </section>
   );
 }
